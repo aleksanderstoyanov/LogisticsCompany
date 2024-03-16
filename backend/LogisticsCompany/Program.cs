6ae8d02829e2977c94fe19070a379629e9b6b31e
aleksanderstoyanov <blackeder3@gmail.com>
2024-03-16 14:03:42 +0200

    chore(db): create initial user and role tables

--- backend/LogisticsCompany/Program.cs
+++ backend/LogisticsCompany/Program.cs
@@ -1,8 +1,12 @@
+using LogisticsCompany.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddSingleton<LogisticsCompanyContext>();
 
 var app = builder.Build();
 
@@ -13,5 +17,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapControllerRoute(name: "default", "api/[controller]/{action}/{id?}");
 
 app.Run();
