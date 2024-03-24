90f6d7f4d00a0c3ef0aa7b317f9e159dc9216a5e
aleksanderstoyanov <blackeder3@gmail.com>
2024-03-24 15:47:09 +0200

    chore(db): introduce sql command and query helpers

---
+++ backend/LogisticsCompany/Data/Helpers/SqlCommandHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace LogisticsCompany.Data.Helpers
+{
+    public static class SqlCommandHelper
+    {
+        /// <summary>
+        /// Constructs SQL Insert command with an arbitrary amount of parameters. 
+        /// </summary>
+        /// <param name="table"></param>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        public static string InsertCommand(string table, params string [] values)
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine(string.Format("INSERT INTO {0} VALUES", table));
+            sb.AppendLine("(");
+            sb.Append(String.Join(", ", values));
+            sb.AppendLine(")");
+
+            return sb.ToString().Trim();
+        }
+    }
+
+}
---
+++ backend/LogisticsCompany/Data/Helpers/SqlQueryHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace LogisticsCompany.Data.Helpers
+{
+    public class SqlQueryHelper
+    {
+        /// <summary>
+        /// Constructs an SQL query for retrieving all records.
+        /// </summary>
+        /// <param name="table"></param>
+        /// <returns></returns>
+        public static string SelectAll(string table)
+            => string.Format("SELECT * FROM {0}", table);
+
+        /// <summary>
+        /// Constructs an SQL query for retrieving the count of all records.
+        /// </summary>
+        /// <param name="table"></param>
+        /// <returns></returns>
+        public static string SelectAllCount(string table)
+            => string.Format("SELECT COUNT(Id) FROM {0}", table);
+    }
+}
