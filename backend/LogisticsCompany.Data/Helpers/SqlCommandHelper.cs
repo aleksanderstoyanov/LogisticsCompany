2435a8f591b897994fe7391a3d016fee0925f713
aleksanderstoyanov <blackeder3@gmail.com>
2024-03-30 10:55:47 +0200

    chore(db): refactor insert command in sql command helper

--- backend/LogisticsCompany.Data/Helpers/SqlCommandHelper.cs
+++ backend/LogisticsCompany.Data/Helpers/SqlCommandHelper.cs
@@ -8,17 +8,16 @@ namespace LogisticsCompany.Data.Helpers
         /// <summary>
         /// Constructs SQL Insert command with an arbitrary amount of parameters. 
         /// </summary>
-        /// <param name="table"></param>
-        /// <param name="values"></param>
-        /// <returns></returns>
+        /// <param name="table">The table to which values will be added.</param>
+        /// <param name="values">Values for the table.</param>
         public static string InsertCommand(string table, params string [] values)
         {
             var sb = new StringBuilder();
 
-            sb.AppendLine(string.Format("INSERT INTO {0} VALUES", table));
-            sb.AppendLine("(");
-            sb.Append(String.Join(", ", values));
-            sb.AppendLine(")");
+            sb.AppendLine(string.Format("INSERT INTO {0} VALUES", table))
+              .AppendLine("(")
+              .Append(string.Join(", ", values))
+              .AppendLine(")");
 
             return sb.ToString().Trim();
         }
