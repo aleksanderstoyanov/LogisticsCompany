7797ec8b140281164bb80268ca7e475ea15d83da
aleksanderstoyanov <blackeder3@gmail.com>
2024-09-29 16:54:05 +0300

    chore(db): introduce sql db adapter

---
+++ backend/LogisticsCompany.Data/Common/Adapters/SqlDbAdapter.cs
@@ -0,0 +1,43 @@
+﻿using Dapper;
+using LogisticsCompany.Data.Contracts;
+using Microsoft.Data.SqlClient;
+
+namespace LogisticsCompany.Data.Common.Adapters
+{
+    public class SqlDbAdapter : IDbAdapter
+    {
+        private readonly string _connectionString;
+
+        public SqlDbAdapter(string connectionString)
+        {
+            this._connectionString = connectionString;
+        }
+
+        public async Task<T> QuerySingle<T>(string query)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var result = await connection.QuerySingleOrDefaultAsync<T>(query);
+                return result;
+            }
+        }
+
+        public async Task<IEnumerable<T>> QueryAll<T>(string query)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var result = await connection.QueryAsync<T>(query);
+                return result;
+            }
+        }
+
+        public async Task ExecuteCommand(string command, object? param = null)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.ExecuteAsync(command, param);
+            }
+        }
+      
+    }
+}
---
+++ backend/LogisticsCompany.Data/Contracts/IDbAdapter.cs
@@ -0,0 +1,9 @@
+﻿namespace LogisticsCompany.Data.Contracts
+{
+    public interface IDbAdapter
+    {
+        Task<T> QuerySingle<T>(string query);
+        Task<IEnumerable<T>> QueryAll<T>(string query);
+        Task ExecuteCommand(string command, object? param = null);
+    }
+}
