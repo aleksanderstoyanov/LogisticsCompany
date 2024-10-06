19f4bc6434026476c8bc85858e50b0e98d02155a
aleksanderstoyanov <blackeder3@gmail.com>
2024-10-06 15:32:18 +0300

    chore(db): add summaries to sqlDbAdapter

--- backend/LogisticsCompany.Data/Common/Adapters/SqlDbAdapter.cs
+++ backend/LogisticsCompany.Data/Common/Adapters/SqlDbAdapter.cs
@@ -8,11 +8,22 @@ namespace LogisticsCompany.Data.Common.Adapters
     {
         private readonly string _connectionString;
 
+        /// <summary>
+        /// Constructs the SqlAdapter with the passed <paramref name="connectionString"/>.
+        /// </summary>
+        /// <param name="connectionString">
+        /// The connection string that will make connectivity to the database.
+        /// </param>
         public SqlDbAdapter(string connectionString)
         {
             this._connectionString = connectionString;
         }
 
+        /// <summary>
+        /// Executes a single row query.
+        /// </summary>
+        /// <typeparam name="T">The return type that the query will be mapped to.</typeparam>
+        /// <param name="query">The constructed query.</param>
         public async Task<T> QuerySingle<T>(string query)
         {
             using (var connection = new SqlConnection(_connectionString))
@@ -22,6 +33,11 @@ namespace LogisticsCompany.Data.Common.Adapters
             }
         }
 
+        /// <summary>
+        /// Execute a multiple row query.
+        /// </summary>
+        /// <typeparam name="T">The return type that the query will be mapped to.</typeparam>
+        /// <param name="query">The constructed query.</param>
         public async Task<IEnumerable<T>> QueryAll<T>(string query)
         {
             using (var connection = new SqlConnection(_connectionString))
@@ -31,6 +47,11 @@ namespace LogisticsCompany.Data.Common.Adapters
             }
         }
 
+        /// <summary>
+        /// Executes a single command.
+        /// </summary>
+        /// <param name="command">The command that will be executed.</param>
+        /// <param name="param">The additional parameters that will be passed to the command.</param>
         public async Task ExecuteCommand(string command, object? param = null)
         {
             using (var connection = new SqlConnection(_connectionString))
