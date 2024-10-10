6573d1eff65a5ab62e89a36d48979ff35be3e271
aleksanderstoyanov <blackeder3@gmail.com>
2024-10-10 17:11:12 +0300

    test(services, api): introduce base service test class

---
+++ backend/LogisticsCompany.Tests/Services/BaseServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using Autofac.Core;
+using LogisticsCompany.Data.Contracts;
+using Microsoft.Extensions.Configuration;
+using Moq;
+
+namespace LogisticsCompany.Tests.Services
+{
+    public abstract class BaseServiceTest
+    {
+        protected readonly Mock<IDbAdapter> _dbAdapter;
+        protected readonly Parameter[] _dbParameters;
+
+        public BaseServiceTest()
+        {
+            var inMemorySettings = new Dictionary<string, string> {
+                    {"ConnectionStrings:DefaultConnectionString", "Test"},
+                };
+
+            var configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(inMemorySettings)
+                .Build();
+
+            _dbAdapter = new Mock<IDbAdapter>();
+            _dbParameters = new Parameter[1]
+            {
+                new TypedParameter(typeof(IConfiguration), configuration)
+            };
+        }
+    }
+}
